b7e20547129fb96031b37145e4a64e2cf592f40c
Tilak <112251445+tilakjain619@users.noreply.github.com>
2025-02-02 17:07:54 +0530

    Create FinancialAdvisor.java

---
+++ src/main/java/com/wellsfargo/counselor/entity/FinancialAdvisor.java
@@ -0,0 +1,63 @@
+package com.example.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class FinancialAdvisor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long advisorId;
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    @OneToMany(mappedBy = "financialAdvisor")
+    private List<Client> clients;
+
+    // Constructor
+    public FinancialAdvisor(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters and Setters
+    public Long getAdvisorId() {
+        return advisorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+}
